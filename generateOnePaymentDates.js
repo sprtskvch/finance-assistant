5b92bafecaaea9b13d6aa6d09329cf652fb895dd
Turbobot <turbobot@vercel.com>
2025-04-06 16:35:53 +0300

    feat(create-turbo): apply official-starter transform

--- generateOnePaymentDates.js
+++
@@ -1,3 +0,0 @@
-export function generateOnePaymentDates({ startDate }) {
-  return [startDate];
-}
--- printPaymentDates.js
+++
@@ -1,5 +0,0 @@
-export function printPaymentDates(paymentDates) {
-    paymentDates.forEach((date) => {
-      console.log(date.format("DD.MM.YYYY"));
-    });
-  }
